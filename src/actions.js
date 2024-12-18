d160b7ff0885e526009dc96648d97d06350ced5d
michel-lewis <kouamkouammichellewis@gmail.com>
2024-12-18 15:43:29 +0100

    fix: history modification table

--- src/actions.js
+++ src/actions.js
@@ -91,6 +91,7 @@ export function fetchCheckModificationHistory(filters) {
                     }
                 }
                 pageInfo {
+                endCursor
                 hasNextPage
                 hasPreviousPage
                 startCursor
